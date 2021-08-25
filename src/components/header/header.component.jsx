6a1752fbc50fbc400d345ee0fa4268d60b4c78fe
nicolassso <nicolasleal1996@gmail.com>
2021-08-25 17:51:58 +0200

    deploying marcha

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
+import './header.styles.scss'
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+const Header = ({currentUser, hidden}) => (
+
+    <div className="header">
+        <Link className='logo-container' to='/'>
+            <Logo className='logo'></Logo>
+        </Link>
+        <div className="options">
+            <Link className="option" to='/shop' >
+                OBRAS
+            </Link>
+            <Link className="option" to='/shop' >
+                CONTACTO
+            </Link>
+            {
+                currentUser ? (
+                    <div className="option" onClick={()=>auth.signOut()}>
+                        REGISTRO
+                    </div>
+                ) : (
+                    <Link className='option' to='/signin'>
+                        SALIR
+                    </Link>
+                )
+            }
+            <CartIcon />
+        </div>
+        {
+            hidden ? null : <CartDropdown />
+        }
+    </div>
+
+);
+
+const mapStateToProps = ({user: { currentUser }, cart: {hidden}}) => ({
+    currentUser,
+    hidden
+})
+
+
+export default connect(mapStateToProps)(Header);
