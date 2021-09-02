1e401e8305dc2bd053b7a0f213788eb81fc410c8
nicolassso <nicolasleal1996@gmail.com>
2021-09-02 14:39:07 +0200

    filled de colecciones-data.js file

--- src/pages/shop/colecciones.data.jsx
+++ src/pages/shop/colecciones.data.jsx
@@ -1,59 +1,350 @@
 const COLECCIONES_DATA = [
     {
       id: 1,
-      title: 'Exposiciones',
-      routeName: 'exposiciones',
-      items: [
-        {
-          id: 4,
-          name: 'Marlborough',
-          city: 'New York',
-          year: ["1990, ", "1992"]
-        },
-        {
-          id: 5,
-          name: 'Hill Club',
-          city: 'Marbella',
-          year: ["1994, ", "1997"]
-        }
-      ]
-    },
-    {
-      id: 2,
       title: 'Coleccionistas',
       routeName: 'coleccionistas',
       items: [
         {
-          id: 6,
-          name: 'Lady Diana',
-          city: 'London',
-          year: 1980
+          id: 1,
+          name: 'MARQUESES DE ENGLOFFTEING',
+          city: 'Alemania'
         },
         {
-          id: 7,
-          name: 'Antonio Banderas',
-          city: 'Málaga',
-          year: 2020
+          id: 2,
+          name: 'DODO CUNMEINHAM-REID',
+          city: 'Inglaterra'
         },
+        {
+          id: 41,
+          name: 'VON BISMARCK',
+          city: 'Alemania'
+        },
+        {
+          id: 42,
+          name: 'MARQUESES DE PAUL-LARIOS',
+          city: 'España'
+        },
+        {
+          id: 43,
+          name: 'FELICE CULTRERA',
+          city: 'Italia'
+        },
+        {
+          id: 44,
+          name: 'PRINCIPE IRANI ABBDELAH',
+          city: 'Francia'
+        },
+        {
+          id: 45,
+          name: 'CAMILO SEXTO',
+          city: 'España'
+        },
+        {
+          id: 46,
+          name: 'MICHEL STARK',
+          city: 'Estados Unidos'
+        },
+        {
+          id: 47,
+          name: 'SRS. KALACHMIKOFF',
+          city: 'Londres'
+        },
+        {
+          id: 48,
+          name: 'DUQUESA DE SEVILLA',
+          city: 'Miami'
+        },
+        {
+          id: 49,
+          name: 'MARQUESES DE SAN DAMIAN',
+          city: 'Londres'
+        },
+        {
+          id: 50,
+          name: 'SRS. GONZALVES PEREIRA',
+          city: 'Portugal'
+        },
+        {
+          id: 51,
+          name: 'ASMAWI',
+          city: 'Marbella'
+        },
+        {
+          id: 52,
+          name: 'HILLEVI SOMMERHALDER',
+          city: 'Londres'
+        },
+        {
+          id: 53,
+          name: 'NAYMAH BENJELLOUM',
+          city: 'Marruecos'
+        },
+        {
+          id: 54,
+          name: 'ABDELLATIF EL HARIZI',
+          city: 'Marruecos'
+        },
+        {
+          id: 55,
+          name: 'THYSSEN',
+          city: 'España'
+        },
+        {
+          id: 56,
+          name: 'TERCIO DUQUE DE ALBA',
+          city: 'Ceuta'
+        },
+        {
+          id: 57,
+          name: 'Subasta SOTHEBY\'S',
+          city: 'Estados Unidos',
+          year: ["1991 ", "- 1992"]
+        },
+        {
+          id: 58,
+          name: 'Palacio de Lebrija',
+          city: 'Sevilla'
+        }
 
       ]
     },
+    {
+      id: 2,
+      title: 'Exposiciones',
+      routeName: 'exposiciones',
+      items: [
+        {
+          id: 3,
+          name: 'Galería DURAN',
+          city: 'Madrid',
+          year: ["1973 ", "- 2001 ", "- 2002 ", "- 2018"]
+        },
+        {
+          id: 4,
+          name: 'Palacio de Congresos y Arte Contemporáneo',
+          city: 'Marbella',
+          year: ["2006 ", "- 2014 ", "- 2017"]
+        },
+        {
+          id: 5,
+          name: 'Palacio de Congresos y Arte Contemporáneo',
+          city: 'Sevilla',
+          year: ["2003 ", "- 2004 ", "- 2005 ", "- 2016"]
+        },
+        {
+          id: 6,
+          name: 'Marbella CLub',
+          city: 'Marbella',
+          year: ["1995 ", "- 2010 ", "- 2013 ", "- 2015"]
+        },
+        {
+          id: 7,
+          name: 'ACAF. Melbourne',
+          city: 'Melbourne',
+          year: ["2003 ", "- 2012 "]
+        },
+        {
+          id: 8,
+          name: 'MUSEO DE CEUTA, Conjunto Monumental Murallas Reales',
+          city: 'Ceuta',
+          year: ["2001 ", "- 2011 "]
+        },
+        {
+          id: 9,
+          name: 'JULIAN & SIMON',
+          city: 'Londres',
+          year: ["1990 ", "- 1991 ", "- 2009 ", "- 2011"]
+        },
+        {
+          id: 10,
+          name: 'Galería MARLBOROUGH',
+          city: 'New York',
+          year: ["1990 ", "- 2008 "]
+        },
+        {
+          id: 11,
+          name: 'Fondation OMAR BEMJELLOUN',
+          city: 'Marrakech',
+          year: ["2006 ", "- 2007 "]
+        },
+        {
+          id: 12,
+          name: 'Casa Real de Marruecos. Colección particular de su Majestad el Rey MOHAMMED VI y S.A.R. SALMA BEMMANY ',
+          city: 'Marrakech',
+          year: ["2004 "]
+        },
+        {
+          id: 13,
+          name: 'Palacio de Congresos y Arte Contemporáneo',
+          city: 'Málaga',
+          year: ["2003 ", "- 2004 "]
+        },
+        {
+          id: 14,
+          name: 'ARTEXPOO, Feria Internacional de San Francisco',
+          city: 'San Francisco',
+          year: ["2000 "]
+        },
+        {
+          id: 15,
+          name: 'ARTE AMERICA, Feria Internacional de Arte de New York',
+          city: 'New York',
+          year: ["2000 "]
+        },
+        {
+          id: 16,
+          name: 'FABIEN FRYNS',
+          city: 'Marbella',
+          year: ["1997 "]
+        },
+        {
+          id: 17,
+          name: 'Galería EL BOLLANDER',
+          city: 'Palm Beach',
+          year: ["1995 ", "- 2000 "]
+        },
+        {
+          id: 18,
+          name: 'Jardines Colgantes',
+          city: 'Marbella',
+          year: ["1989 "]
+        },
+        {
+          id: 19,
+          name: 'Galería MAC INTOCH',
+          city: 'Marrakech',
+          year: ["1989 "]
+        },
+        {
+          id: 20,
+          name: 'ANALCAI',
+          city: 'Madrid',
+          year: ["1988 ", "- 1999 "]
+        },
+        {
+          id: 21,
+          name: 'Galería SOKOA',
+          city: 'Madrid',
+          year: ["1987 ", "- 1990 ", "- 1997 "]
+        },
+        {
+          id: 22,
+          name: 'Las Kapellanias, Expo. Particular',
+          city: 'Marbella',
+          year: ["1986 "]
+        },
+        {
+          id: 23,
+          name: 'RODEO BEACH CLUCH',
+          city: 'Marbella',
+          year: ["1985 ", "- 1986 "]
+        },
+        {
+          id: 24,
+          name: 'Galería TRIANGULO',
+          city: 'París',
+          year: ["1983 "]
+        },
+        {
+          id: 25,
+          name: 'Sala PETIT MEROU',
+          city: 'Tetuán',
+          year: ["1983 ", "- 1984"]
+        },
+        {
+          id: 26,
+          name: 'Galería SAANEN ',
+          city: 'Gstaad (Suiza)',
+          year: ["1982 "]
+        },
+        {
+          id: 27,
+          name: 'Palacio de Lebrija',
+          city: 'Sevilla',
+          year: ["1982 "]
+        },
+        {
+          id: 28,
+          name: 'Casa de Cultura',
+          city: 'Melilla',
+          year: ["1981 ", "- 1982"]
+        },
+        {
+          id: 29,
+          name: 'Sala RIBALTA de Galerías S. Vicente',
+          city: 'Valencia',
+          year: ["1981 "]
+        },
+        {
+          id: 30,
+          name: 'Galería SERRANO 19',
+          city: 'Madrid',
+          year: ["1979 "]
+        },
+        {
+          id: 31,
+          name: 'Casa de Cultura',
+          city: 'Ceuta',
+          year: ["1978 "]
+        },
+        {
+          id: 32,
+          name: 'Sala MIRAMAR',
+          city: 'Málaga',
+          year: ["1977 ", "- 1980"]
+        },
+        {
+          id: 33,
+          name: 'Centro Hijos de Ceuta',
+          city: 'Ceuta',
+          year: ["1976 "]
+        },
+        {
+          id: 34,
+          name: 'Casa de Cultura de Gétale',
+          city: 'Madrid',
+          year: ["1975 "]
+        },
+        {
+          id: 35,
+          name: 'CASA YAMGUAS',
+          city: 'Granada',
+          year: ["1975 "]
+        },
+        {
+          id: 36,
+          name: 'CASINO MILITAR',
+          city: 'Ceuta',
+          year: ["1974 ", "- 1984", "- 1985"]
+        },
+        {
+          id: 37,
+          name: 'SOPPIN',
+          city: 'Laredo, Santander',
+          year: ["1972 "]
+        },
+        {
+          id: 38,
+          name: 'KABILA',
+          city: 'Tetuán',
+          year: ["1971 "]
+        }
+      ]
+    },    
     {
       id: 3,
       title: 'Museos',
       routeName: 'museos',
       items: [
         {
-          id: 8,
-          name: 'Marqueses de San Damián',
-          city: 'Londres',
-          year: 1995
+          id: 39,
+          name: 'Museo Casa de Cultura de Arjonilla',
+          city: 'Jaén'
         },
         {
-          id: 9,
-          name: 'Palacio de Lebrija',
-          city: 'Sevilla',
-          year: 1999
+          id: 40,
+          name: 'Museo Ayuntamiento de Ceuta',
+          city: 'Ceuta'
         }
       ]
     }
