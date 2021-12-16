02e2c9fdcfe8bb89eb1d182118150a30eedffc6f
nicolassso <nicolasleal1996@gmail.com>
2021-12-16 17:50:24 +0100

    implemented API, getting data from MySQL server, need to move this to separeted component

--- src/pages/under-construct/under-construct-page.jsx
+++ src/pages/under-construct/under-construct-page.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import './under-construct-page.styles.scss'
 
-const UnderConstruct = () => (
+
+
+const UnderConstruct = () => {
+
+
+    //getting data from MySQL !! it is working omg
+
+    const [cuadros, setCuadros] = useState([])
+
+    const getDataFromApi = async () => {
+        try {
+            const res = await axios.get("http://localhost:3001/api/cuadros")
+            setCuadros(res.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getDataFromApi()    
+    }, [])
+    console.log(cuadros)
+
+    
+
+    return(
     <div className="under-construct">
         <span className="text"><p className="dot-typing">...</p></span>
     </div>
-)
+    )
+}
+
 
 export default UnderConstruct;
