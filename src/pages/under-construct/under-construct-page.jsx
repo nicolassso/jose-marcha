416c4c0d2dd7bf3b9d75be28a1fb179c32563257
nicolassso <nicolasleal1996@gmail.com>
2022-01-06 14:13:46 +0100

    cloudinary dependency, access to images easily

--- src/pages/under-construct/under-construct-page.jsx
+++ src/pages/under-construct/under-construct-page.jsx
@@ -1,3 +1,4 @@
+import { CloudinaryContext, Image } from 'cloudinary-react'
 import React from 'react'
 
 import './under-construct-page.styles.scss'
@@ -8,12 +9,17 @@ const UnderConstruct = ({cuadros}) => {
     return(
     <div className="under-construct">
         <span className="text"><p className="dot-typing">...</p></span>
+        <CloudinaryContext cloudName="dn2k6sthd">
+            <div>
+                <Image publicId="https://res.cloudinary.com/dn2k6sthd/image/upload/v1641471647/marcha/abstractos/abstracto-volcan-3_ycoesg.jpg" width="800" />
+            </div>
+        </CloudinaryContext>
         {
         cuadros.map(cuadro => {
             return(
                 <div key={cuadro.id} style={{
                     margin: '20px',
-                    textAlign: 'center'
+                    textAlign: 'center' 
                 }}>
                     <h1>{cuadro.name}</h1>
                     <h2>{cuadro.author}</h2>
